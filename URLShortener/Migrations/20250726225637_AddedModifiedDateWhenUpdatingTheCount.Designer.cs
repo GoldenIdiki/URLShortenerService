283aeef23d4fec84d5cc4ec28eb58d342c2fa6e8
Golden Idikibiebuma <golden.idiki@gmail.com>
2025-07-26 23:58:07 +0100

    Added modified date

--- URLShortener/Migrations/20250726225637_AddedModifiedDateWhenUpdatingTheCount.Designer.cs
+++ URLShortener/Migrations/20250726225637_AddedModifiedDateWhenUpdatingTheCount.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using URLShortener.Data;
@@ -11,9 +12,11 @@ using URLShortener.Data;
 namespace URLShortener.Migrations
 {
     [DbContext(typeof(URLShortenerDbContext))]
-    partial class URLShortenerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250726225637_AddedModifiedDateWhenUpdatingTheCount")]
+    partial class AddedModifiedDateWhenUpdatingTheCount
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ URLShortener/Migrations/20250726225637_AddedModifiedDateWhenUpdatingTheCount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace URLShortener.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedModifiedDateWhenUpdatingTheCount : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateModified",
+                table: "UrlMappings",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateModified",
+                table: "UrlMappings");
+        }
+    }
+}
